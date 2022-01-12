656ffc9547018f2754334dffc89a7dd9483fcfac
avel-front <vp.kalashnikov@mail.ru>
2022-01-12 17:48:20 +0300

    added new console.log

--- app.js
+++ app.js
@@ -1,2 +1,3 @@
 document.body.innerHTML = '<h1>Hello from JavaScript</h1>'
 console.log('Локально внесены изменения и пока что на GitHub они не видны')
+console.log('Я скачал этот проект через консоль и открыл его, как 3-е лицо')
